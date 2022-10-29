bc6d193284ba13a55e0241707af367e90aa3c215
MattM <matthias.meys@outlook.com>
2022-10-29 14:03:31 +0200

    Navigating between rooms is working!!!!

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Welcome to SyntraZork! Input 'help' for options")
+import sys
+
+print("Welcome to SyntraZork! Input 'help' for options\n")
 
 coor = [0,0,0] #coordinates x,y,z
 roomCoorD = {(0,0,0):"Lobby",(0,0,1):"Stairs0to1"}
@@ -6,35 +8,32 @@ roomCoorD = {(0,0,0):"Lobby",(0,0,1):"Stairs0to1"}
 def list2tuple(x):
     return tuple(x)
 
-def checkIn(ele,target):
-    return True if list2tuple(ele) in target else False
+def str_to_class(str):
+    return getattr(sys.modules[__name__], str)
 
 def roomFromCoor(coor):
     roomCoorKeys = list(roomCoorD.keys())
     roomCoorVals = list(roomCoorD.values())
-    position = roomCoorKeys.index(coor)
-    currentRoom = roomCoorVals[position]
+    position = roomCoorKeys.index(list2tuple(coor))
+    currentRoom = str_to_class(roomCoorVals[position])
     return currentRoom
 
 class Room:
-    def __init__(self,roomName,roomCoor,shortT,longT,lookL,goL):
+    def __init__(self,roomName,shortT,longT,lookL,goL):
         self.roomName = roomName
-        self.roomCoor = roomCoor
         self.longT = longT
         self.shortT = shortT
         self.lookL = lookL
         self.goL = goL
 
     def inRoomCheck(self,ele,rList):
-        if ele not in rList:
-            print(f"There is no {ele} in the {self.roomName}")
+        return True if ele in rList else False
 
     def testThis(self):
         print("test successful")
 
 Lobby=Room(
     roomName = "Lobby",
-    roomCoor = [0,0,0],
     shortT = "You are in the lobby.",
     longT = "To your right is the reception desk."
            " A large display is hanging up high."
@@ -43,6 +42,14 @@ Lobby=Room(
            "There is a bar to your west.",
     lookL = ["reception","bar","display"],
     goL = ["u","s","w"]
+)
+
+Stairs0to1=Room(
+    roomName = "Stairs0to1",
+    shortT = "You are in the stairwell on the first floor.",
+    longT = "STILL HAVE TO WRITE THIS DESCRIPTION",
+    lookL = [],
+    goL = ["d"]
 )
 
 def helpMenu():
@@ -55,15 +62,16 @@ def helpMenu():
           "q to quit\n")
 
 def walkInp():
-#    print(currentRoom.inRoomCheck(ele=))
     if currentRoom.inRoomCheck(inp,currentRoom.goL):
+        print("'ill doo it")
         if inp.lower() == "n": coor[1] = coor[1]+1
         if inp.lower() == "e": coor[0] = coor[0]+1
         if inp.lower() == "s": coor[1] = coor[1]-1
         if inp.lower() == "w": coor[0] = coor[0]-1
         if inp.lower() == "u": coor[2] = coor[2]+1
         if inp.lower() == "d": coor[2] = coor[2]-1
-    else: pass
+        print("it was succesful, coor=",coor)
+    else: print("fuckdat",currentRoom.inRoomCheck(inp,currentRoom.goL))
 
 def optionsInp():
     if inp.lower() == "help": helpMenu()
@@ -72,18 +80,17 @@ def optionsInp():
    # if inp.lower() == "l": printIniT()
 
 while True:
-    print(checkIn(coor,roomCoorD))
+    currentRoom = roomFromCoor(coor)
     try:
-        currentRoom = roomFromCoor(list2tuple(coor))
+        currentRoom = roomFromCoor(coor)
     except:
-        print(f"ERROR! You somehow entered a place that does not exist. Whoops! Coor = {coor}")
-    print("currentroom= ",currentRoom)
+        print(f"ERROR! You somehow got into a place that does not exist. "
+              f"You will be forever stuck at coordinates {coor}."
+              f"Whoops!")
+
+    print("currentroom= ",currentRoom.roomName)
 
     inp = input("\nWhat do you do? ___")
-    print("inp = ",inp)
-    print(Lobby.testThis())
-    print(type(currentRoom))
-    print(currentRoom.testThis())
     if inp.lower() == "q": break
     walkInp()
     optionsInp()
