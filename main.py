802de88ddb461a1447820d96d6856917b41c0581
MattM <matthias.meys@outlook.com>
2022-10-30 08:08:44 +0100

    Room and stair classes done!

--- main.py
+++ main.py
@@ -5,14 +5,11 @@ print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
       "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 
 coor = [0,0,0] #coordinates x,y,z
-roomCoorD = {(0,0,0):"Lobby",
-             (0,1,0):"Stairs0",
-             (0,1,1):"Stairs1",
+roomCoorD = {(0,0,0):"Lobby", #a dictionary with all the rooms in the game
+             (0,1,0):"Stairs0", #this is used to determine which room the player is in based on the
+             (0,1,1):"Stairs1", #coordinates
              (0,1,2):"Stairs2",
              (0,1,3):"Stairs3"}
-
-def invalidAction():
-    print("I don't understand what you want to do.")
 
 def list2tuple(x):
     return tuple(x)
@@ -26,6 +23,21 @@ def roomFromCoor(coor):
     position = roomCoorKeys.index(list2tuple(coor))
     currentRoom = str_to_class(roomCoorVals[position])
     return currentRoom
+
+class Protagonist:
+    def __init__(self,score=0,inv=()):
+        self.score = score
+        self.inv = inv
+
+    def changeScore(self,points):
+        self.score = self.score + points
+        if points < 0:
+            print(f"Your score decreased by {abs(points)} points.\n")
+        else:
+            print(f"Your score increased by {points} points.\n")
+
+    def prtScore(self):
+        print (f"Your score is {self.score}.")
 
 class Room:
     def __init__(self,roomName,shortT,longT,lookL,goL):
@@ -59,11 +71,16 @@ class Stairs(Room):
             nStairsClimbed = 0
         nStairsClimbed+=1
         if nStairsClimbed == 2:
-            print("You are starting to feel winded from walking up the stairs.") #TO-DOSCORE REDUCTION
+            print("You are starting to feel winded from walking up the stairs.")
+            Player.changeScore(-1)
         if nStairsClimbed == 3:
-            print("Climbing the stairs is starting to make you sweat.") #TO-DOSCORE REDUCTION
+            print("Climbing the stairs is starting to make you sweat.")
+            Player.changeScore(-5)
         if nStairsClimbed > 3:
-            print(f"You've climbed {nStairsClimbed} stairs... Your programmer's muscles ache.")#TO-DOSCORE REDUCTION
+            print(f"You've climbed {nStairsClimbed} stairs... Your programmer's muscles ache.")
+            Player.changeScore(-10)
+
+Player = Protagonist()
 
 Lobby=Room(
     roomName = "Lobby",
@@ -109,8 +126,9 @@ Stairs2=Stairs(
 
 Stairs3=Stairs(
     roomName = "Stairs3",
-    shortT = "You are near the stairs on the third floor. This is the top floor.",
-    longT = " The hallway to your west has multiple classrooms.\n"
+    shortT = "You are near the stairs on the third floor.",
+    longT = " This is the top floor.\n"
+            " The hallway to your west has multiple classrooms.\n"
             " You can take the steps back down to the second floor.",
     lookL = [],
     goL = ["w","d"]
@@ -123,6 +141,8 @@ def helpMenu():
           "w to go West\n"
           "u to go Up\n"
           "d to go Down\n"
+          "look to look around the room\n"
+          "score to display your current score\n"
           "q to quit\n")
 
 def walkInp():
@@ -147,29 +167,36 @@ def walkInp():
         print(f"You cannot go that way.")
         return True,currentRoom
 
-def optionsInp():
+def optionsInp(): #various meta-options
     if inp.lower() == "help":
         helpMenu()
         return True
 
-    if inp.lower() == "coor":
-        print(currentRoom.roomName,coor)
+    if inp.lower() in ["coor","where","room","location"]:
+        print(f"{currentRoom.shortT}. Your current coordinates are {coor}.")
         return True
-    return False #no valid "options" input given
 
-def lookInp():
+    if inp.lower() in ["score","points"]:
+        Player.prtScore()
+        return True
+
+    return False
+
+def lookInp(): #looking around the room
     lookSyn = ["look", "l", "info", "room", roomFromCoor(coor)]
+
     if inp.lower() in lookSyn:
         currentRoom.lookRoom(),
         return True
     else:
         return False #no valid "look" input given
 
+def invalidAction(): #this is called when player inputs nonsense
+    print("I don't understand what you want to do.")
+
 gameStart = True
 while True:
-
-    #setting up the very first room at the start of the game...
-    if gameStart == True:
+    if gameStart == True:    #setting up the very first room at the start of the game...
         print("------------------------------------------------------------------------------------------------------")
         currentRoom = roomFromCoor(coor)
         currentRoom.enterRoom()
